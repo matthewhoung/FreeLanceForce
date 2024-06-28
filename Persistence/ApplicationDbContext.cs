9f080b62acd78feb407ac17a9b9c2ff69210e724
DESKTOP-FTVMTJF\user <matthewhoung@gmail.com>
2024-06-28 11:04:50 +0800

    ApplicationDbContext update, repo down as template.

--- Persistence/ApplicationDbContext.cs
+++ Persistence/ApplicationDbContext.cs
@@ -1,12 +1,18 @@
 ﻿using Domain.Forms;
 using Microsoft.EntityFrameworkCore;
+/*
+ * 預先從NuGet安裝Microsoft.EntityFrameworkCore
+ *               Microsoft.EntityFrameworkCore.Design
+ *               Microsoft.EntityFrameworkCore.Tools
+ * 之後打開NuGet Package Manager Console
+ * 輸入: Add-Migration InitialCreate
+ *       Update-Database
+ */
 
 namespace FreeLanceForce.Persistence
 {
     public class ApplicationDbContext : DbContext
     {
-        public DbSet<Form> Forms { get; set; }
-
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseNpgsql("Server=localhost;Port=5432;User Id=postgres;Password=20240401;Database=VPlus_dev;");
@@ -14,7 +20,6 @@ namespace FreeLanceForce.Persistence
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            base.OnModelCreating(modelBuilder);
             modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
         }
     }
